e8d4633dd1e551c965f467a3d25f63e446851fb9
Philip Williams <williamsphilip@outlook.com>
2021-10-20 17:10:16 +0100

    Create add_car template

--- app.py
+++ app.py
@@ -100,6 +100,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_car")
+def add_car():
+   return render_template("add_car.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
